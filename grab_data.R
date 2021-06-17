ff34beb98c086a1a3454f3a8f1077cf9061b865f
Varun Mittal <varunaops@gmail.com>
2021-06-16 19:31:29 -0500

    initial version with raw_data

---
+++ grab_data.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(rvest)
+library(janitor)
+library(lubridate)
+
+
+x <- read_html("https://en.wikipedia.org/wiki/UEFA_Euro_2020_squads")
+
+raw_data <- x %>% 
+   html_elements("td:nth-child(4)") %>% 
+  html_text2() %>% 
+  as_tibble()
+
+
